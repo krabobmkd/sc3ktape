23d6435be64cd608cdee5eb335fb98f74be55e2a
krabobmkd <victorien.ferry@free.fr>
2022-12-05 21:54:42 +0100

    assemble with wla-z80

--- asmtest/bitmap.asm
+++ asmtest/bitmap.asm
@@ -13,50 +13,81 @@
 ;res (reset) bit a zero
 ; bit (btst , flag z) 
 
-VDPData: equ $be
-
-CopyToVDP: macro
-	ld c, VDPData
--:
-	outi
-	dec de
-	ld a,d
-	or e
-	jp nz,-
-	endm
+.memorymap
+defaultslot 0
+slotsize $ffff
+slot 0 $0000
+;slot 1 $4000
+;slot 2 $8000
+.endme
 
 
-;.macro CopyToVDP
+.rombankmap
+bankstotal 1
+banksize $ffff
+banks 1
+.endro
+
+.define VDPControl $bf
+.define VDPData $be
+.define VDPScanline $7e
+.define PSGPort $7f
+.define IOPortA $DC
+.define VRAMRead $0000
+.define VRAMWrite $4000
+.define CRAMWrite $c000
+
+
+.macro SetVDPAddress args addr  ; c36
+; sets the VDP address
+    ld a, <addr
+    out (VDPControl),a
+    ld a, >addr
+    out (VDPControl),a
+.endm
+
+.macro CopyToVDP
 ; copies data to the VDP
 ; parameters: hl = data address, de = data length
 ; affects: a, hl, bc, de
-;    ld c, VDPData
-;-:  outi
-;    dec de
-;    ld a,d
-;    or e
-;    jp nz,-
-;.endm
+    ld c, VDPData
+-:  outi ; Reads from (HL) and writes to the (C) port. HL is then incremented, and B is decremented.
+    dec de
+    ld a,d
+    or e
+    jp nz,-
+.endm
 
-	org $980F
+;.bank 1 slot 1
+	.org $980F
 testd:
 main:
 	di
 
-	CopyToVDP
+    ; load tiles (Background)
+;    SetVDPAddress $0000 | VRAMWrite
+;    ld hl, FiftyTiles
+;    ld de, FiftyTilesSize
+;    CopyToVDP
+
+    ; load tilemap (Background)
+    SetVDPAddress $3800 | VRAMWrite
+    ld hl,testdata
+    ld de,dataend-testdata
+    CopyToVDP
+
+
 	ei
-	jp justafter ; jump adress are 2b absolute according to org.
-	nop
-justafter:
-	call other ; calls adress are 2b absoluete according to org.
+;	jp justafter ; jump adress are 2b absolute according to org.
+;	nop
+;justafter:
+;	call other ; calls adress are 2b absoluete according to org.
 	ret
-other:
-	ret
+;other:
+;	ret
 ; - - - - -
 testdata:
-	db "CAN WE"
+	.db "CAN WE"
 dataend:
-	db 0
-	db $13
-	db $37
-	db 0
+	.db $10
+	.db $11
